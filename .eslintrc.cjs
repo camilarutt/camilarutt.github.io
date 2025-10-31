603bf909ac7ad0bbe64057a1f083558c8fe16dcd
Camila Ruttimann <camilarut@gmail.com>
2025-10-31 16:50:48 -0300

    Created main files and folders for a more organized project. Started inserting scripts to facilitate the development.

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+  plugins: ["@typescript-eslint", "react"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  settings: { react: { version: "detect" } },
+};
